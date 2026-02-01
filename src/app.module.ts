5cecaf0342ed335d5a7af55704da6950a3157caf
Sayan Sen <sayansen0361@gmail.com>
2026-02-01 21:18:47 +0530

    Load env variables in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi'
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [HelloModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes the configmodule globally available. helps to load .env files
+      // validationSchema: joi.object({ 
+      //   APP_NAME : joi.string().default('defaultApp'), // but here I'm using app.config.ts for the validation stuff
+      // }),
+      load: [appConfig],
+  }), HelloModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/app.config.ts
@@ -0,0 +1,4 @@
+export default () => ({
+    appName: process.env.APP_NAME || "NestjsApp",
+    // remaining variables (load them here)
+})
